757ecb9e521a23a1bbb68ed5971476d4bf99d773
Frank <zongpengchen@gmail.com>
2022-11-07 22:31:23 +0800

    feat: make api work for backend

--- ToDoFrontEnd/src/app/api/todo.api.service.ts
+++ ToDoFrontEnd/src/app/api/todo.api.service.ts
@@ -12,15 +12,17 @@ export class TodoApiService {
   constructor(private http: HttpClient) { }
 
   create(todoItem: ToDoItem): Observable<void> {
-    return this.http.post<void>('https://635fc244ca0fe3c21aa3d012.mockapi.io/api/todos', todoItem);
-    // return this.http.post<void>('https://localhost:5001/api/todos', todoItem);
+    // return this.http.post<void>('https://635fc244ca0fe3c21aa3d012.mockapi.io/api/todos', todoItem);
+    return this.http.post<void>('https://localhost:5001/api/todos', todoItem);
   }
 
   update(todoItem: ToDoItem): Observable<void> {
-    return this.http.put<void>(`https://635fc244ca0fe3c21aa3d012.mockapi.io/api/todos/${todoItem.id}`, todoItem);
+    // return this.http.put<void>(`https://635fc244ca0fe3c21aa3d012.mockapi.io/api/todos/${todoItem.id}`, todoItem);
+    return this.http.put<void>(`https://localhost:5001/api/todos/${todoItem.id}`, todoItem);
   }
 
   delete(id: number): Observable<void> {
-    return this.http.delete<void>(`https://635fc244ca0fe3c21aa3d012.mockapi.io/api/todos/${id}`);
+    // return this.http.delete<void>(`https://635fc244ca0fe3c21aa3d012.mockapi.io/api/todos/${id}`);
+    return this.http.delete<void>(`https://localhost:5001/api/todos/${id}`);
   }
 }

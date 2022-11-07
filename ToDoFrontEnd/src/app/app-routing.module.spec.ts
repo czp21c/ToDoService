d9628f846f133434d5621477a7f04fa7e828b75f
Frank <zongpengchen@gmail.com>
2022-11-07 14:48:12 +0800

    feat: add routers

--- ToDoFrontEnd/src/app/app-routing.module.spec.ts
+++ ToDoFrontEnd/src/app/app-routing.module.spec.ts
@@ -2,6 +2,13 @@ import { Location } from '@angular/common';
 import { TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { Router } from '@angular/router';
+import { routes } from './app-routing.module';
+/**
+ * /todos
+ * /todos/create
+ * /todos/edit/:id
+ * /todos/:id details
+ */
 
 describe('Router', () => {
   let location: Location;
@@ -9,7 +16,7 @@ describe('Router', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule.withRoutes([])],
+      imports: [RouterTestingModule.withRoutes(routes)],
       declarations: []
     });
 
@@ -19,7 +26,39 @@ describe('Router', () => {
     router.initialNavigation();
   });
 
-  it('test navigate', fakeAsync(() =>{
+  it('should navigate todos when navigate ""', fakeAsync(() =>{
+    // given
+    // when
+    router.navigate([''])
+    tick(50)
+    // then
+    expect(location.path()).toEqual('/todos')
+  }));
 
+  it('should navigate todos//edit/:id when navigate "todos/edit/:id"', fakeAsync(() =>{
+    // given
+    // when
+    router.navigate(['todos', 'edit', 2])
+    tick(50)
+    // then
+    expect(location.path()).toEqual('/todos/edit/2')
+  }));
+
+  it('should navigate todos/:id when navigate "todos/:id"', fakeAsync(() =>{
+    // given
+    // when
+    router.navigate(['todos', 2])
+    tick(50)
+    // then
+    expect(location.path()).toEqual('/todos/2')
+  }));
+
+  it('should navigate todos/create when navigate "todos/create"', fakeAsync(() =>{
+    // given
+    // when
+    router.navigate(['todos/create'])
+    tick(50)
+    // then
+    expect(location.path()).toEqual('/todos/create')
   }));
 });
